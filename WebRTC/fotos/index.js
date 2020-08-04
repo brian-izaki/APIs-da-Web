24277b30f2544350229db167f8222f14af238b68
Brian_Izaki <bkizaki@outlook.com>
2020-08-04 10:10:51 -0300

    add funcionalidade de recuperar img do localStorage

--- WebRTC/fotos/index.js
+++ WebRTC/fotos/index.js
@@ -1,49 +1,63 @@
 'use strict'
 
 const video = document.querySelector('video');
-const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector('canvas');
 const btn = document.querySelector('button');
+const localImage = document.querySelector('#local');
+const imgSalvo = document.querySelector('#imgSalvo');
+
+// recupera uma imagem que esteja salvo no localStorage
+localImage.onclick = function(){
+
+  console.log(window.localStorage.getItem('imagem'))
+  // é criado uma tag image
+  const imgRecuperado = new Image();
+  // é adicionado ao src dele o URI recuperado do localStorage
+  imgRecuperado.src = window.localStorage.getItem('imagem');
+  // é colocado dentro da div
+  imgSalvo.appendChild(imgRecuperado)
   
+}
 
 // outra maneira de baixar seria utilizando uma tag a, na qual passa o URL no src e dps a.dowload 
-btn.onclick = function (e){
-  console.log(video.__proto__)
-  console.log(typeof video.width)
-  console.log(video.videoHeight)
+btn.onclick = function (){
+  console.log('__proto__', video.__proto__)
+  console.log('height', video.videoHeight)
 
   // fazer o canvas ter msm tamnaho do video
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
+  // renderizar imagem
   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-
+  
   // transforma o canvas em URL para que possa armazenar ele
-  const img = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+  const img = canvas.toDataURL("image/png", 0.1) //.replace("image/png", "image/octet-stream");
   // window.location.href = img; // baixa um arquivo sem extesão por causa do MIME octet-stream
   window.localStorage.setItem("imagem", img)
   console.log(img)
 }
-
-canvas.width = 500;
-canvas.height = 300;
 
 const constraints = {
   audio: false,
   video: true,
 };
 
-// com esta promisse o stream retorna um objeto 
-// MEDIASTRAM (ela possui propriedades relacionadas com o video)
-navigator.mediaDevices.getUserMedia(constraints)
-  .then( stream => {
-    console.log(stream)
+function handleSucces(stream){
+  console.log('Promisse -> stream', stream)
     // instancia um MediaStream
     window.stream = stream;
     // srcObject é propriedade de HTMLMediaElement (elementos de midia no HTML) carrega um src na tag video
     video.srcObject = stream;
-  })
-  .catch(error => {
-    console.log("mensagem de erro : ", error.message, error.name)
-    if (error.message === "Permission denied"){
-      Permissions
-    }
-  })
+}
+
+function handleError(error){
+  console.log("mensagem de erro : ", error.message, error.name)
+  if (error.message === "Permission denied"){
+    // poderia utilizar a API nativa "Permissions" (https://caniuse.com/#feat=permissions-api) 
+    
+  }
+}
+
+// com esta promisse o stream retorna um objeto 
+// MEDIASTREAM (ela possui propriedades relacionadas com o video)
+navigator.mediaDevices.getUserMedia(constraints).then(handleSucces).catch(handleError)
