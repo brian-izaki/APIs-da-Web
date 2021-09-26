a8ba95219fbe95832915ebae37ea592d5a768133
Brian Izaki <bkizaki@outlook.com>
2021-09-26 14:35:04 -0300

    webcomponents modularizado

--- WebComponents/slots/index.js
+++ WebComponents/slots/index.js
@@ -1,12 +1,35 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+    :host {
+        display: flex;
+        flex-direction: column;
+        background-color: #778;
+        color: white;
+        padding: 1em;
+        margin: 1em;
+    }
+</style>
+
+<slot name="titulo">
+    <h2>Este é o título default</h2>
+</slot>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quibusdam doloribus quia dolor, atque omnis
+    officia est minima doloremque fuga corrupti quae ipsum?</p>
+
+<slot name="conteudo"></slot>
+
+<slot></slot>
+`
+
 class HelloWorldElement extends HTMLElement {
     constructor() {
         super();
 
         this.attachShadow({ mode: 'open' })
 
-        const tpl = document.getElementById('template-wc');
-
-        this.shadowRoot.append(tpl.content.cloneNode(true));
+        this.shadowRoot.append(template.content.cloneNode(true));
     }
 }
 
